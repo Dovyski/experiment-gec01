7c840488f2d5a04fc6052e6fa4892e17054d85d0
Fernando Bevilacqua <dovyski@gmail.com>
2020-05-13 12:20:39 -0300

    Adjust according to locale since base url

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 
-header('Location: ./welcome/');
+
+$aLocale = '';
+$aLocale = isset($_REQUEST['en']) ? 'en' : $aLocale;
+$aLocale = isset($_REQUEST['pt']) ? 'pt' : $aLocale;
+$aLocale = isset($_REQUEST['locale']) ? $_REQUEST['locale'] : $aLocale;
+
+$aURLAppend = !empty($aLocale) ? '?locale=' . $aLocale : '';
+$aURL = 'Location: ./welcome/' . $aURLAppend;
+
+header($aURL);
 exit();
 
 ?>
